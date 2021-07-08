446788317e4c2ef1a107bacf85642177985c0d51
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-08 21:59:01 +0700

    ADD emiting event

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
+const axios = require('axios')
 
 const app = express()
 
@@ -15,13 +16,22 @@ app.get('/posts/:id/comments', (req, res) => {
 	res.send(commentsByPostId[postId] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
 	const commentId = randomBytes(4).toString('hex')
 	const { content } = req.body
 	const postId = req.params.id
 	const comments = commentsByPostId[postId] || []
 	comments.push({ id: commentId, content })
 	commentsByPostId[postId] = comments
+
+	await axios.post('http://localhost:4005/event', {
+		type: 'CommentCreated',
+		data: {
+			id: commentId,
+			postId: postId,
+			content,
+		},
+	})
 
 	res.status(201).send(comments)
 })
