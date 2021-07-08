ffefab5ba130b2ecc73316d3da3ece47e0703756
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-08 22:40:06 +0700

    Remove unused import

--- blog/client/src/components/CommentList.js
+++ blog/client/src/components/CommentList.js
@@ -1,5 +1,4 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
