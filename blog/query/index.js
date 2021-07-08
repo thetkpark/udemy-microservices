ede2e569a39ead3f16cd317829325bb0c7e3959f
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-08 22:35:22 +0700

    ADD event handler

--- blog/query/index.js
+++ blog/query/index.js
@@ -6,9 +6,24 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/posts', (req, res) => {})
+const posts = {}
 
-app.post('/events', (req, res) => {})
+app.get('/posts', (req, res) => {
+	res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+	const { type, data } = req.body
+	if (type == 'PostCreated') {
+		const { id, title } = data
+		posts[id] = { id, title, comments: [] }
+	}
+	if (type == 'CommentCreated') {
+		const { id, postId, content } = data
+		posts[postId].comments.push({ id, content })
+	}
+	res.send({})
+})
 
 app.listen(4002, () => {
 	console.log('Listening on 4002')
