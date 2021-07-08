effd6edcdbcd6f290fbc2ff53cdcac1c1896e1ae
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-08 21:49:27 +0700

    ADD /event impl

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -1,6 +1,19 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const axios = require("axios")
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
+
+app.post('/event', (req, res) => {
+	const event = req.body
+	axios.post('http://localhost:4000', event)
+	axios.post('http://localhost:4001', event)
+	axios.post('http://localhost:4002', event)
+
+	res.send({ status: 'OK' })
+})
+
+app.listen(4005, () => {
+	console.log('Listening on 4005')
+})
