5abbf90a9a22dfdbf84f1f2bfbe4b8858a62fc65
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-09 16:30:42 +0700

    ADD K8S

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -24,7 +24,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 	comments.push({ id: commentId, content, status: 'pending' })
 	commentsByPostId[postId] = comments
 
-	await axios.post('http://localhost:4005/events', {
+	await axios.post('http://event-bus-srv:4005/events', {
 		type: 'CommentCreated',
 		data: {
 			id: commentId,
@@ -45,7 +45,7 @@ app.post('/events', async (req, res) => {
 		const comment = comments.find(el => el.id == id)
 		comment.status = status
 
-		await axios.post('http://localhost:4005/events', {
+		await axios.post('http://event-bus-srv:4005/events', {
 			type: 'CommentUpdated',
 			data: {
 				...data,
