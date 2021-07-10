14c7e4a487d0734d6e5beda3ec74621fafa14fdb
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-10 14:05:02 +0700

    ADD connection to mongo

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import { json } from 'body-parser'
+import 'express-async-errors'
+import mongoose from 'mongoose'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
 import { NotFoundError } from './errors/not-found-error'
-import 'express-async-errors'
 
 const app = express()
 
@@ -23,6 +24,20 @@ app.all('*', async () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-	console.log('Listening on port 3000')
-})
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		})
+		console.log('Connected to auth MongoDB')
+	} catch (error) {
+		console.error(error)
+	}
+	app.listen(3000, () => {
+		console.log('Listening on port 3000')
+	})
+}
+
+start()
