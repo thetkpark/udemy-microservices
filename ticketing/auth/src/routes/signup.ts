af1f3c758bc3355878cc07a8140af33d03f507a1
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-10 20:35:22 +0700

    ADD validate request

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator'
 import jwt from 'jsonwebtoken'
 import { BadRequestError } from '../errors/bad-request-error'
 import { RequestValidationError } from '../errors/request-validation-error'
+import { validateRequest } from '../middlewares/validate-request'
 import { User } from '../models/user'
 import { Password } from '../services/password'
 
@@ -17,12 +18,8 @@ router.post(
 			.isLength({ min: 4, max: 20 })
 			.withMessage('Password must be between 4 and 20 characters'),
 	],
+	validateRequest,
 	async (req: Request, res: Response) => {
-		const errors = validationResult(req)
-		if (!errors.isEmpty()) {
-			throw new RequestValidationError(errors.array())
-		}
-
 		const { email, password } = req.body
 
 		const existingUser = await User.findOne({ email })
