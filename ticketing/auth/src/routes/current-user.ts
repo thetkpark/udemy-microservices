ca001def6074f416b40165362af5697bb86990e2
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-10 20:44:03 +0700

    ADD current user

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (req, res) => {
-	res.send('Hi There')
+	if (!req.session?.jwt) {
+		return res.send({ currentUser: null })
+	}
+
+	const jwtSecret = process.env.JWT_KEY!
+	try {
+		const payload = jwt.verify(req.session.jwt, jwtSecret)
+		res.send({ currentUser: payload })
+	} catch (error) {
+		res.send({ currentUser: null })
+	}
 })
 
 export { router as currentUserRouter }
