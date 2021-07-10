f781e84110aff1e05086175e85cba785c778a7ab
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-10 14:34:41 +0700

    ADD badRequestError

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -3,7 +3,7 @@ import { CustomError } from '../errors/custom-error'
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 	if (err instanceof CustomError) {
-		return res.status(err.statusCode).send(err.serializeErrors())
+		return res.status(err.statusCode).send({ errors: err.serializeErrors() })
 	}
 
 	res.status(500).send({ errors: [{ message: 'Something went wrong' }] })
