5f9d6282e8117157f9058b894fb2126968fb2efa
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-10 20:39:21 +0700

    ADD signin logic

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import jwt from 'jsonwebtoken'
+import { BadRequestError } from '../errors/bad-request-error'
 import { validateRequest } from '../middlewares/validate-request'
+import { User } from '../models/user'
+import { Password } from '../services/password'
 
 const router = express.Router()
 
@@ -11,7 +15,25 @@ router.post(
 		body('password').trim().notEmpty().withMessage('You must supply a password'),
 	],
 	validateRequest,
-	(req: Request, res: Response) => {}
+	async (req: Request, res: Response) => {
+		const { email, password } = req.body
+		const user = await User.findOne({ email })
+		if (!user) throw new BadRequestError('Invalid credentials')
+
+		const passwordMatch = await Password.compare(user.password, password)
+		if (!passwordMatch) throw new BadRequestError('Invalid credential')
+
+		const jwtSecret = process.env.JWT_KEY!
+		const token = await jwt.sign({ id: user.id, email: user.email }, jwtSecret, {
+			algorithm: 'HS256',
+		})
+
+		req.session = {
+			jwt: token,
+		}
+
+		res.status(200).send(user)
+	}
 )
 
 export { router as signinRouter }
