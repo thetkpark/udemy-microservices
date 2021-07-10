d5d1364705a8f11e7381b44a56f07a3449b5f667
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-10 11:40:55 +0700

    ADD error handler

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { RequestValidationError } from '../errors/request-validation-error'
 
 const router = express.Router()
 
@@ -15,9 +17,10 @@ router.post(
 	(req: Request, res: Response) => {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
-			return res.status(400).send(errors.array())
+			throw new RequestValidationError(errors.array())
 		}
-		const { email, password } = req.body
+		// const { email, password } = req.body
+		throw new DatabaseConnectionError()
 		res.send('Hi There')
 	}
 )
