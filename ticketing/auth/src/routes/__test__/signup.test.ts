fff3a692b46228dbb0fc314785c36118807add07
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-11 11:01:27 +0700

    FINISH testing signup

--- ticketing/auth/src/routes/__test__/signup.test.ts
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -1,3 +1,4 @@
+import { response } from 'express'
 import request from 'supertest'
 import { app } from '../../app'
 
@@ -9,4 +10,49 @@ it('return a 201 on successful signup', async () => {
 			password: 'testPassword',
 		})
 		.expect(201)
+})
+
+it('return 400 with an invalid email', async () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'invalideAtmaildotcom',
+			password: 'testPassword',
+		})
+		.expect(400)
+})
+
+it('return 400 with an invalid password', async () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'valid@email.com',
+			password: '123',
+		})
+		.expect(400)
+})
+
+it('return 400 with missing email and password', async () => {
+	await request(app).post('/api/users/signup').send({ email: 'valid@email.com' }).expect(400)
+	await request(app).post('/api/users/signup').send({ password: 'testPassword' }).expect(400)
+})
+
+it('disallow duplicate emails', async () => {
+	const reqBody = {
+		email: 'valid@email.com',
+		password: 'testPassword',
+	}
+	await request(app).post('/api/users/signup').send(reqBody).expect(201)
+	await request(app).post('/api/users/signup').send(reqBody).expect(400)
+})
+
+it('sets a cookie after successful signup', async () => {
+	const res = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'valid@email.com',
+			password: 'testPassword',
+		})
+		.expect(201)
+	expect(res.get('Set-Cookie')).toBeDefined()
 })
