8941c096841248b3ad36c56602666d27bf9b459e
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-11 17:22:26 +0700

    ADD getServerSideProps

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,24 @@
-const Home = () => {
-	return <h1>Landing Page</h1>
+import axios from 'axios'
+
+const Home = ({ currentUser }) => {
+  return <h1>{currentUser.email}</h1>
+}
+
+export async function getServerSideProps({ req }) {
+  try {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      { headers: req.headers }
+    )
+    return {
+      props: data,
+    }
+  } catch (error) {
+    console.error(error)
+  }
+  return {
+    props: {},
+  }
 }
 
 export default Home
