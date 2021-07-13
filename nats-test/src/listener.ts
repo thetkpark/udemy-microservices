a209419ad3b0f42608d9bc17935f35494e1b4072
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-13 17:22:52 +0700

    DONE

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,7 +15,11 @@ stan.on('connect', () => {
 		process.exit()
 	})
 
-	const options = stan.subscriptionOptions().setManualAckMode(true)
+	const options = stan
+		.subscriptionOptions()
+		.setManualAckMode(true)
+		.setDeliverAllAvailable()
+		.setDurableName('orders-services')
 
 	// Queue Group of each event ->Only one listener get the event (No duplicate event)
 	const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group', options)
