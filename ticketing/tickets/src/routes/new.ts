c8bd393f3c78b9b9aa157a3b7086f3381dd87067
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-14 14:36:16 +0700

    ADD client get

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
 import { Ticket } from '../models/ticket'
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
@@ -23,12 +24,12 @@ router.post(
 		})
 		await ticket.save()
 
-		// new TicketCreatedPublisher(client).publish({
-		// 	id: ticket.id,
-		// 	title: ticket.title,
-		// 	price: ticket.price,
-		// 	userId: ticket.userId,
-		// })
+		new TicketCreatedPublisher(natsWrapper.client).publish({
+			id: ticket.id,
+			title: ticket.title,
+			price: ticket.price,
+			userId: ticket.userId,
+		})
 
 		res.status(201).send(ticket)
 	}
