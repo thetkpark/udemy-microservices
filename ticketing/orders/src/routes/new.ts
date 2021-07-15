7643ba92f72065292815e845d21ac7d07dc700ff
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-15 12:03:36 +0700

    ADD validation

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,13 +1,20 @@
 import express, { Request, Response } from 'express'
 import { requireAuth, validateRequest } from '@sp-udemy-ticketing/common'
 import { body } from 'express-validator'
+import mongoose from 'mongoose'
 
 const router = express.Router()
 
 router.post(
 	'/api/orders',
 	requireAuth,
-	[body('ticketId').notEmpty().withMessage('ticketId must be provided')],
+	[
+		body('ticketId')
+			.notEmpty()
+			.custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+			.withMessage('ticketId must be provided'),
+	],
+	validateRequest,
 	async (req: Request, res: Response) => {
 		res.send({})
 	}
