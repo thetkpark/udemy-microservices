88da46b1845d6b281fac4b91dcec6dc4d58f60c0
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-16 12:55:12 +0700

    ADD version check to TicketUpdate

--- ticketing/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -8,7 +8,7 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
 	readonly queueGroupName = queueGroupName
 
 	async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
-		const { id, title, price } = data
+		const { id, title, price, version } = data
 		const ticket = Ticket.build({ id, title, price })
 		await ticket.save()
 
--- ticketing/orders/src/events/listeners/ticket-updated-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,8 +8,11 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
 	readonly queueGroupName = queueGroupName
 
 	async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-		const { id, title, price } = data
-		const ticket = await Ticket.findById(id)
+		const { id, title, price, version } = data
+		const ticket = await Ticket.findOne({
+			id,
+			version: version - 1,
+		})
 		if (!ticket) throw new Error('Ticket not found')
 
 		ticket.title = title
