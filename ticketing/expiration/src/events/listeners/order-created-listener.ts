2692d75b66dcbfbf4ce2fe6841f446879eac443d
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-17 13:22:06 +0700

    ADD order create listener and bull queue

---
+++ ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,14 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@sp-udemy-ticketing/common'
+import { Message } from 'node-nats-streaming'
+import { queueGroupName } from './queue-group-name'
+import { expirationQueue } from '../../queues/expiration-queue'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+	readonly subject = Subjects.OrderCreated
+	readonly queueGroupName = queueGroupName
+
+	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+		await expirationQueue.add({ orderId: data.id })
+		msg.ack()
+	}
+}
--- ticketing/expiration/src/index.ts
+++ ticketing/expiration/src/index.ts
@@ -1,6 +1,8 @@
+import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { natsWrapper } from './nats-wrapper'
 
 const start = async () => {
+	if (!process.env.REDIS_HOST) throw new Error('REDIS_HOST must be defined')
 	if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID must be defined')
 	if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined')
 	if (!process.env.NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID must be defined')
@@ -15,7 +17,7 @@ const start = async () => {
 		process.on('SIGINT', () => natsWrapper.client.close())
 		process.on('SIGTERM', () => natsWrapper.client.close())
 
-
+		new OrderCreatedListener(natsWrapper.client).listen()
 	} catch (error) {
 		console.error(error)
 	}
