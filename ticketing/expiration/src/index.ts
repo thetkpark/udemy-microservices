b5e280d6549844a7a215f583f06095e9b2599e2e
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-17 12:42:29 +0700

    Remove express and mongo

--- ticketing/expiration/src/index.ts
+++ ticketing/expiration/src/index.ts
@@ -1,12 +1,6 @@
-import mongoose from 'mongoose'
-import { app } from './app'
-import { OrderCancelledListener } from './events/listeners/order-cancelled-listener'
-import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { natsWrapper } from './nats-wrapper'
 
 const start = async () => {
-	if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined')
-	if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined')
 	if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID must be defined')
 	if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined')
 	if (!process.env.NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID must be defined')
@@ -21,22 +15,10 @@ const start = async () => {
 		process.on('SIGINT', () => natsWrapper.client.close())
 		process.on('SIGTERM', () => natsWrapper.client.close())
 
-		new OrderCreatedListener(natsWrapper.client).listen()
-		new OrderCancelledListener(natsWrapper.client).listen()
 
-		// MongoDB connection
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-		})
-		console.log('Connected to tickets MongoDB')
 	} catch (error) {
 		console.error(error)
 	}
-	app.listen(3000, () => {
-		console.log('Listening on port 3000')
-	})
 }
 
 start()
