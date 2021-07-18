1ee3c9e5622dfd158d7478f188a89bf7e568fb3f
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-18 15:02:12 +0700

    ADD publish PaymentCreatedPublisher

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import { body } from 'express-validator'
 import { stripe } from '../stripe'
 import { Order } from '../models/order'
 import { Payment } from '../models/payment'
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
@@ -39,7 +41,13 @@ router.post(
 		const payment = Payment.build({ orderId, stripeId })
 		await payment.save()
 
-		res.status(201).send({ success: true })
+		new PaymentCreatedPublisher(natsWrapper.client).publish({
+			id: payment.id,
+			orderId,
+			stripeId,
+		})
+
+		res.status(201).send({ id: payment.id })
 	}
 )
 
