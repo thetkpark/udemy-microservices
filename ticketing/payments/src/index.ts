08bc99a8149ddf1709aed2df6b1fc41dc40b226a
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-18 13:12:08 +0700

    ADD stripe.ts

--- ticketing/payments/src/index.ts
+++ ticketing/payments/src/index.ts
@@ -10,7 +10,8 @@ const start = async () => {
 	if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID must be defined')
 	if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined')
 	if (!process.env.NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID must be defined')
-
+	if (!process.env.STRIPE_KEY) throw new Error('STRIPE_KEY must be defined')
+	
 	try {
 		// NATS Connection
 		await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
