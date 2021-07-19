be6823bba517332a97c75f268c8f14995003c3ed
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-19 10:21:54 +0700

    ADD listing ticket

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,19 +1,45 @@
 import axiosBuilder from '../api/build-client'
 
-const Home = ({ currentUser }) => {
-  return <div>{currentUser ? <h1>You are sign in</h1> : <h1>You are not sign in!!</h1>}</div>
+const Home = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  )
 }
 export async function getServerSideProps({ req }) {
   try {
-    const { data } = axiosBuilder(req).get('/api/tickets')
+    const { data } = await axiosBuilder(req).get('/api/tickets')
     return {
-      props: data,
+      props: {
+        tickets: data,
+      },
     }
   } catch (error) {
     console.error(error)
   }
   return {
-    props: {},
+    props: {
+      tickets: [],
+    },
   }
 }
 
